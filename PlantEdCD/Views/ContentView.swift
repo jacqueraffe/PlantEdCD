41aae67b8e9171dbfb036c3e58141872f66d480d
Jacqueline Palevich <jacqueline.palevich@gmail.com>
2021-09-06 11:09:52 -0700

    updates app icon

--- PlantEdCD/Views/ContentView.swift
+++ PlantEdCD/Views/ContentView.swift
@@ -27,6 +27,11 @@ struct ContentView: View {
                     Image(systemName: "list.dash")
                     Text("Types")
                 }
+            aboutTab
+                .tabItem{
+                    Image(systemName: "gear")
+                    Text("About")
+                }
         }
     }
     
@@ -52,6 +57,21 @@ struct ContentView: View {
             TypeList()
             Text("Swipe right to see the type list.")
         }
+    }
+    
+    var aboutTab: some View {
+        aboutText.frame(maxWidth: 300)
+    }
+    
+    @ViewBuilder
+    var aboutText: some View {
+        Text("""
+I wrote this app for my Independent Studies class. I hope it helps you all to \
+remember to water your plants! :)
+
+Plant information provided by wikipedia.org/wiki/Houseplant under the \
+Creative Commons Attribution-ShareAlike License.
+""")
     }
     
 }
